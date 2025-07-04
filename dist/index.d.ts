930c69bb48b6acf3a4db7f09e79236faa1c70a25
WindyPear-Team <fireguo@flweb.cn>
2025-07-04 22:47:36 +0800

    init1

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context, Config, ConfigSchema } from 'yumeri';
-export declare const depend: string[];
-export declare const usage = "Oauth\u670D\u52A1\u7AEF\uFF0C\u5BF9\u63A5\u4E0A\u6E38OIDC\u670D\u52A1\u7AEF\u3002<br>\u91CD\u5B9A\u5411\u94FE\u63A5\u8BF7\u5199\\<\u57DF\u540D\\>/oauth/callback";
-export declare const config: {
-    schema: Record<string, ConfigSchema>;
-};
-export declare function apply(ctx: Context, config: Config): Promise<void>;
--- dist/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function generateCode(length?: number): string;
-export declare function getUserInfo(userinfoEndpoint: string, accessToken: string): Promise<any>;
-export declare function getAccess(tokenEndpoint: string, clientId: string, clientSecret: string, scope?: string): Promise<any>;
-export declare function refreshAccessToken(tokenEndpoint: string, clientId: string, clientSecret: string, refreshToken: string): Promise<any>;
